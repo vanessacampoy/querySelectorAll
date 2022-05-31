69a3298dbd66b2891c6acd67f63b5151d6e781af
Vanessa Campoy <88167544+vanessacampoy@users.noreply.github.com>
2022-05-31 14:58:32 -0300

    Add files via upload

---
+++ querySelectorAll/scripts.js
@@ -0,0 +1,14 @@
+const lista = ["vermelho", "azul", "amarelo", "laranja"]
+lista.pop()//remove o último item da array
+
+const ultimo = lista.pop()//tira o primeiro item da lista
+const primeiro = lista.shift()//tira o último item da lista
+
+console.log(primeiro)
+console.log(ultimo)
+
+lista.push("rosa")//acrescenta um item a lista
+console.log(lista)
+
+
+
